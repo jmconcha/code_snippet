235304a3da1f2653edb8b8f1c8f025b712b87c39
jmconcha <jmconcha.work@gmail.com>
2020-11-18 21:57:08 +0800

    Refactor 011-form-validation

--- JavaScript/Projects/011-form-validation/scripts/main.js
+++ JavaScript/Projects/011-form-validation/scripts/main.js
@@ -2,6 +2,36 @@
 
 window.addEventListener('load', () => {
 
-    
+    const registerForm = document.forms['registerForm'];
+
+    registerForm.onsubmit = function(e) {
+        e.preventDefault();
+
+        alert('Fields Validated and Valid');
+    };
+
+    registerForm['fullName'].oninput = function () {
+        this.setCustomValidity('');
+
+        if (!this.checkValidity()) {
+            this.setCustomValidity('Alphabets only.');
+        }
+    };
+
+    registerForm['zipCode'].oninput = function () {
+        this.setCustomValidity('');
+
+        if (!this.checkValidity()) {
+            this.setCustomValidity('Numeric only.');
+        }
+    };
+
+    const selectCountry = registerForm['country'];
+    for (const country of countries) {
+        const option = document.createElement('option');
+        option.setAttribute('value', country);
+        option.textContent = country;
+        selectCountry.appendChild(option);
+    }
 
 });
